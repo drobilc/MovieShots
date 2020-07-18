f0df1421fd9b25f114fbaa7666ef902b0bea6e67
Niki Bizjak <drobilc@gmail.com>
2020-07-18 13:53:22 +0200

    Add bonus words to the end of the generated game

--- app/src/main/java/com/moviedrinkers/moviedrinkers/adapter/GameDisplayAdapter.kt
+++ app/src/main/java/com/moviedrinkers/moviedrinkers/adapter/GameDisplayAdapter.kt
@@ -6,16 +6,35 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.moviedrinkers.moviedrinkers.R
+import com.moviedrinkers.moviedrinkers.data.DrinkingCue
 import com.moviedrinkers.moviedrinkers.data.DrinkingGame
 import com.moviedrinkers.moviedrinkers.data.DrinkingGamePlayer
+import kotlinx.android.synthetic.main.list_item_bonus_word.view.*
 import kotlinx.android.synthetic.main.list_item_game_display.view.*
+import kotlinx.android.synthetic.main.list_item_game_display.view.words
 import kotlinx.android.synthetic.main.list_item_game_intro.view.*
+
+abstract class Item
+class Intro: Item()
+class PlayerCue(val position: Int, val player: DrinkingGamePlayer): Item()
+class BonusIntro: Item()
+class BonusCue(val position: Int, var drinkingCue: DrinkingCue): Item()
+class Spacer: Item()
 
 class GameDisplayAdapter(private val game: DrinkingGame): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    companion object {
-        private const val TYPE_INTRO = 1
-        private const val TYPE_CUE = 2
+    private var items: ArrayList<Item> = arrayListOf()
+
+    init {
+        this.items.add(Intro())
+        for ((index, player) in game.players.withIndex())
+            this.items.add(PlayerCue(index + 1, player))
+
+        this.items.add(BonusIntro())
+        for ((index, bonusWord) in game.bonusWords.withIndex())
+            this.items.add(BonusCue(index, bonusWord))
+
+        this.items.add(Spacer())
     }
 
     class CueViewHolder(view: View) : RecyclerView.ViewHolder(view) {
@@ -26,7 +45,7 @@ class GameDisplayAdapter(private val game: DrinkingGame): RecyclerView.Adapter<R
 
         fun bind(drinkingGamePlayer: DrinkingGamePlayer, position: Int, itemCount: Int) {
             words.text = drinkingGamePlayer.getWords()
-            player.text = itemView.context.getString(R.string.player_number, position + 1)
+            player.text = itemView.context.getString(R.string.player_number, position)
 
             val occurrences = drinkingGamePlayer.getTotalOccurrences()
             intoxication.text = itemView.context.resources.getQuantityString(R.plurals.intoxication_level_shots, occurrences, occurrences)
@@ -52,39 +71,74 @@ class GameDisplayAdapter(private val game: DrinkingGame): RecyclerView.Adapter<R
         }
     }
 
+    class BonusIntroViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        fun bind(game: DrinkingGame) {
+
+        }
+    }
+
+    class BonusCueViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val leftColorStrip = view.color_strip_left
+        private val word = view.word
+
+        fun bind(cue: BonusCue, itemCount: Int) {
+            word.text = cue.drinkingCue.word
+
+            val hue = cue.position * (360.0f / itemCount)
+            val stripColor = Color.HSVToColor(floatArrayOf(hue, 0.6f, 1.0f))
+            leftColorStrip.setBackgroundColor(stripColor)
+        }
+    }
+
+    class SpacerViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        fun bind() {}
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        if (viewType == TYPE_INTRO)
-            return IntroViewHolder(
-                LayoutInflater.from(parent.context).inflate(
-                    R.layout.list_item_game_intro,
-                    parent,
-                    false
-                )
-            )
-        return CueViewHolder(
-            LayoutInflater.from(parent.context).inflate(
-                R.layout.list_item_game_display,
-                parent,
-                false
-            )
-        )
+        val inflater = LayoutInflater.from(parent.context)
+        return when (viewType) {
+            TYPE_INTRO -> IntroViewHolder(inflater.inflate(R.layout.list_item_game_intro, parent, false))
+            TYPE_CUE -> CueViewHolder(inflater.inflate(R.layout.list_item_game_display, parent, false))
+            TYPE_BONUS_WORDS_INTRO -> BonusIntroViewHolder(inflater.inflate(R.layout.list_item_bonus_intro, parent, false))
+            TYPE_BONUS_WORD -> BonusCueViewHolder(inflater.inflate(R.layout.list_item_bonus_word, parent, false))
+            TYPE_SPACER -> SpacerViewHolder(inflater.inflate(R.layout.list_item_spacer, parent, false))
+            else -> CueViewHolder(inflater.inflate(R.layout.list_item_game_display, parent, false))
+        }
     }
 
     // Replace the contents of a view (invoked by the layout manager)
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        if (getItemViewType(position) == TYPE_INTRO) {
-            (holder as IntroViewHolder).bind(this.game)
-        } else {
-            val player: DrinkingGamePlayer = game.players[position - 1]
-            (holder as CueViewHolder).bind(player, position - 1, itemCount)
+        val item = this.items[position]
+        when (getItemViewType(position)) {
+            TYPE_INTRO -> (holder as IntroViewHolder).bind(this.game)
+            TYPE_CUE -> (holder as CueViewHolder).bind((item as PlayerCue).player, item.position, game.players.size)
+            TYPE_BONUS_WORDS_INTRO -> (holder as BonusIntroViewHolder).bind(this.game)
+            TYPE_BONUS_WORD -> (holder as BonusCueViewHolder).bind((item as BonusCue), game.bonusWords.size)
+            TYPE_SPACER -> (holder as SpacerViewHolder).bind()
         }
     }
 
-    override fun getItemCount() = game.players.size + 1
+    override fun getItemCount() = this.items.size
 
     override fun getItemViewType(position: Int): Int {
-        if (position == 0)
+        val item = this.items[position]
+        if (item is Intro)
             return TYPE_INTRO
-        return TYPE_CUE
+        if (item is PlayerCue)
+            return TYPE_CUE
+        if (item is BonusIntro)
+            return TYPE_BONUS_WORDS_INTRO
+        if (item is BonusCue)
+            return TYPE_BONUS_WORD
+        return TYPE_SPACER
     }
+
+    companion object {
+        private const val TYPE_INTRO = 1
+        private const val TYPE_CUE = 2
+        private const val TYPE_BONUS_WORDS_INTRO = 4
+        private const val TYPE_BONUS_WORD = 8
+        private const val TYPE_SPACER = 16
+    }
+
 }
